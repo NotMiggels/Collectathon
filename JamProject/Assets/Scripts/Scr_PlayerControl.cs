3c1598d0ae3ccfda8a328b889a9fcee2916e42ca
Jeff Yang <www.speedway@hotmail.com>
2019-01-31 00:50:45 -0500

    Added player controls and camera movement

--- JamProject/Assets/Scripts/Scr_PlayerControl.cs
+++ JamProject/Assets/Scripts/Scr_PlayerControl.cs
@@ -5,25 +5,35 @@ using UnityEngine;
 public class Scr_PlayerControl : MonoBehaviour {
 
 
-    public float moveSpeed;
+    public float top_spd; //1
+    public float jump_velo; //20
+    public float accel; //.45
+    public float brake_drag; //10
     private bool playerMoving;
     private Vector2 lastMove;
 
     private Animator anim;
     private Rigidbody2D myRigidbody;
-
+    private CircleCollider2D myCollider;
+    private bool in_air;
+    private bool W_pressed;
+    private float orig_drag;
+   
 
     // Use this for initialization
     void Start () {
-
+        in_air = true;
+        W_pressed = false;
         anim = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<CircleCollider2D>();
 
 
     }
 	
 	// Update is called once per frame
 	void Update () {
+        /*
         playerMoving = false;
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
@@ -51,6 +61,47 @@ public class Scr_PlayerControl : MonoBehaviour {
         anim.SetBool("PlayerMoving", playerMoving);
         anim.SetFloat("LastMoveX", lastMove.x);
         anim.SetFloat("LastMoveY", lastMove.y);
+        */
+        playerMoving = false;
+        
+        if(in_air && (myCollider.IsTouchingLayers(LayerMask.GetMask("Obstacles")) ||
+            myCollider.IsTouchingLayers(LayerMask.GetMask("Platform"))))
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0.0f);
+        }
+        in_air = !(myCollider.IsTouchingLayers(LayerMask.GetMask("Obstacles")) ||
+            myCollider.IsTouchingLayers(LayerMask.GetMask("Platform")));
+       
+        
+        //Debug.Log(in_air);
+        if (Input.GetKey(KeyCode.W) && !in_air && !W_pressed)
+        {
+            W_pressed = true;
+            myRigidbody.AddForce(new Vector2(0.0f, jump_velo));
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            W_pressed = false;
+        }
+        if (Input.GetKey(KeyCode.A) && (myRigidbody.velocity.x) > top_spd*-1.0f)
+        {
+            myRigidbody.AddForce(new Vector2(accel*-1.0f, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.D) && (myRigidbody.velocity.x) < top_spd)
+        {
+            myRigidbody.AddForce(new Vector2(accel, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.S) && !in_air && myRigidbody.velocity.magnitude > 0.0f)
+        {
+            orig_drag = myRigidbody.drag;
+            myRigidbody.drag = brake_drag; 
+        }
+        if (Input.GetKeyUp(KeyCode.S) && !in_air)
+        {
+            myRigidbody.drag = myRigidbody.drag;
+        }
+
+        //not sure what S would actually do
 
     }
 }
---
+++ JamProject/Assets/Scripts/cam_script.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam_script : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject border_l;
+    public GameObject border_r;
+    public GameObject border_u;
+    public GameObject border_d;
+
+    private CircleCollider2D player_collider;
+    private Rigidbody2D my_rb;
+    private Rigidbody2D player_rb;
+	// Use this for initialization
+	void Start () {
+        player_collider = player.GetComponent<CircleCollider2D>();
+        player_rb = player.GetComponent<Rigidbody2D>();
+        my_rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (border_l.GetComponent<BoxCollider2D>().
+            IsTouchingLayers(LayerMask.GetMask("Player")) &&
+            player_rb.velocity.x < 0.0f)
+        {
+            my_rb.velocity = new Vector2(player_rb.velocity.x, 0.0f);
+        }
+        if (border_r.GetComponent<BoxCollider2D>().
+            IsTouchingLayers(LayerMask.GetMask("Player"))
+            && player_rb.velocity.x > 0.0f)
+        {
+            my_rb.velocity = new Vector2(player_rb.velocity.x, 0.0f);
+        }
+        if (border_u.GetComponent<BoxCollider2D>().
+            IsTouchingLayers(LayerMask.GetMask("Player")) &&
+            player_rb.velocity.y > 0.0f)
+        {
+            my_rb.velocity = new Vector2(0.0f, player_rb.velocity.y);
+        }
+        if (border_d.GetComponent<BoxCollider2D>().
+            IsTouchingLayers(LayerMask.GetMask("Player")) &&
+            player_rb.velocity.y < 0.0f)
+        {
+            my_rb.velocity = new Vector2(0.0f, player_rb.velocity.y);
+        }
+        if (!(player_collider.IsTouchingLayers(LayerMask.GetMask("Camera Borders"))))
+        {
+            my_rb.velocity = Vector2.zero;
+        }
+    }
+}
