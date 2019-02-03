f97463bd0a0068eea4654896e8ebfdb120e75f8d
minpark283 <parkm4@rpi.edu>
2019-02-02 23:28:33 -0500

    Added stageload script and walking png

---
+++ JamProject/Assets/Scripts/Scr_StageLoad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scr_StageLoad : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("scene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeScene(string SceneName)
+    {
+        Debug.Log(SceneName);
+        SceneManager.LoadScene(SceneName);
+
+    }
+}
