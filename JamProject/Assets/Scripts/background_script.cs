d90ccb965fc1783c500e063a2f241118730ad4af
minpark283 <parkm4@rpi.edu>
2019-02-10 22:13:22 -0500

    Added Lot of arts

---
+++ JamProject/Assets/Scripts/background_script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_script : MonoBehaviour {
+
+	private float x;
+	private float y;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+
+}
