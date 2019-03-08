6fbb4ad1be81c45ff9f0fed48449d074968f1a49
Jeff Yang <www.speedway@hotmail.com>
2019-03-09 01:32:07 +0900

    added destruction code for jello

---
+++ JamProject/Assets/Scripts/Item Scripts/jello_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jello_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Debug.Log("flingy");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+	}
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hit");
+        Destroy(this.gameObject);
+    }
+
+}
