f9114036305006534cdc1546665741db1e35cc6f
Unknown <scottwrusk@gmail.com>
2019-03-11 15:02:52 -0400

    added some more to Jam Jungle, added the dialogue manager and dialogue facets to the scene, and made a tag for objects Jelly can talk to (I hope our level isn't broken though)

---
+++ JamProject/Assets/Scripts/Character scripts/TalkToCharacter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkToCharacter : MonoBehaviour {
+
+	// if Jelly is in the circle collider trigger of an NPC, the player should be able to talk to them by pressing a key
+    // when those two conditions are met, it should trigger a function on the DialogueTrigger script called TriggerDialogue, which stores the character's dialogue
+    // the trigger dialogue script will then trigger a function on DialogueManager to actually start the dialogue
+}
