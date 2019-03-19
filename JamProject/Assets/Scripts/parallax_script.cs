842e7326f805dca2a5cdb17a85676078b8de7a61
minpark283 <parkm4@rpi.edu>
2019-03-19 01:52:05 -0400

    Made parallax script

--- JamProject/Assets/Scripts/parallax_script.cs
+++ JamProject/Assets/Scripts/parallax_script.cs
@@ -2,16 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class background_script : MonoBehaviour {
+public class parallax_script : MonoBehaviour {
 
-	private float x;
-	private float y;
+	public bool parax;
 
+	public float backgroundSize;
+	public float paralaxSpeed;
+
+	private Transform cameraTransform;
+	private Transform[] layers;
+	private float viewZone = 10;
+	private int leftIndex;
+	private int rightIndex;
+	private float lastCameraX;
+	
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		cameraTransform = Camera.main.transform;
+		lastCameraX = cameraTransform.position.x;
+		layers = new Transform[transform.childCount];
+		for(int i = 0; i < transform.childCount; i++)
+		{
+			layers[i] = transform.GetChild(i);
+		}
+		leftIndex = 0;
+		rightIndex = layers.Length -1;
+
 		
 	}
-	
-	// Update is called once per frame
+
+	private void Update()
+	{
+		float deltaX = cameraTransform.position.x - lastCameraX;
+		transform.position += Vector3.right * (deltaX * paralaxSpeed);
+		lastCameraX = cameraTransform.position.x;
+
+		if(cameraTransform.position.x < (layers[leftIndex].transform.position.x + viewZone))
+		{
+			ScrollLeft();
+		}
+		if(cameraTransform.position.x > (layers[rightIndex].transform.position.x - viewZone))
+		{
+			ScrollRight();
+		}
+	}
+
+	private void ScrollLeft()
+	{
+		int lastLeft = rightIndex;
+		layers[rightIndex].position = Vector3.right *(layers[leftIndex].position.x - backgroundSize);
+		leftIndex = rightIndex;
+		rightIndex --;
+		if(rightIndex < 0)
+		{
+			rightIndex = layers.Length - 1;
+		}
+	}
+
+	private void ScrollRight()
+	{
+		int lastLeft = leftIndex;
+		layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
+		rightIndex = leftIndex;
+		leftIndex --;
+		if(leftIndex == layers.Length)
+		{
+			leftIndex = 0;
+		}
+
+
+	}
 
 }
