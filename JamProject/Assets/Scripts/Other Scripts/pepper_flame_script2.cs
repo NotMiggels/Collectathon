966f9135128bebe3a7bb4b717901a8aa1f48484b
Jeff Yang <www.speedway@hotmail.com>
2019-03-27 01:22:29 -0400

    pepper's flames would deal damage to jelly now

---
+++ JamProject/Assets/Scripts/Other Scripts/pepper_flame_script2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pepper_flame_script2 : MonoBehaviour {
+    private GameObject player;
+    private BoxCollider2D myCollider;
+    private bool damaged;
+	// Use this for initialization
+	void Start () {
+        myCollider = GetComponent<BoxCollider2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        damaged = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Player")) && player != null && !damaged)
+        {
+            damaged = true;
+            player.SendMessage("TakeDMG", 15);
+            //attacking = false;
+            //Debug.Log("player damaged");
+        }
+	}
+}
