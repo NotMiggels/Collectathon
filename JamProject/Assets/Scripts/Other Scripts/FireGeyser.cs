1c85da71086362e076c723b3a3524c9ee822b493
minpark283 <parkm4@rpi.edu>
2019-04-01 02:11:12 -0400

    Added not working fire geyser script

---
+++ JamProject/Assets/Scripts/Other Scripts/FireGeyser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGeyser : MonoBehaviour {
+	public float delaytime;
+	private BoxCollider2D myCollider;
+	private IEnumerator coroutine;
+	private GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+		myCollider = GetComponent<BoxCollider2D>();
+		coroutine = delayaction(delaytime);
+		StartCoroutine(coroutine);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if (myCollider.IsTouchingLayers(LayerMask.GetMask("Player")) && player != null)
+		 {
+			   player.SendMessage("TakeDMG", 10);
+		 }
+	}
+
+
+	private IEnumerator delayaction(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+	}
+
+	private IEnumerator geyserattack(float time)
+	{
+		yield return new WaitForSeconds(time);
+	}
+
+}
