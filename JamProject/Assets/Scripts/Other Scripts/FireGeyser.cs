72305542b8cdd1bf95dad6c80ccb870514f2bbe7
minpark283 <parkm4@rpi.edu>
2019-04-02 06:16:57 -0400

    Fire Geyser works as intended

--- JamProject/Assets/Scripts/Other Scripts/FireGeyser.cs
+++ JamProject/Assets/Scripts/Other Scripts/FireGeyser.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireGeyser : MonoBehaviour {
+	public GameObject[] geyser;
+	public float fire_interv;
+	private int sprite_count;
 	public float delaytime;
 	private BoxCollider2D myCollider;
 	private IEnumerator coroutine;
 	private GameObject player;
+	public float duration;
+	
 
 
 	// Use this for initialization
 	void Start () {
+		sprite_count = geyser.Length;
 		myCollider = GetComponent<BoxCollider2D>();
+        foreach (GameObject flame in geyser){
+            flame.SetActive(false);
+        }
 		coroutine = delayaction(delaytime);
 		StartCoroutine(coroutine);
 
@@ -19,21 +28,42 @@ public class FireGeyser : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		 if (myCollider.IsTouchingLayers(LayerMask.GetMask("Player")) && player != null)
-		 {
-			   player.SendMessage("TakeDMG", 10);
-		 }
 	}
 
 
 	private IEnumerator delayaction(float delay)
 	{
 		yield return new WaitForSeconds(delay);
+		coroutine = geyserattack(0);
+    	StartCoroutine(coroutine);
 	}
 
-	private IEnumerator geyserattack(float time)
+	private IEnumerator geyserattack(int index)
 	{
-		yield return new WaitForSeconds(time);
-	}
+		float deelay = 0;
+        geyser[index].SetActive(true);
+		if(index == sprite_count)
+		{
+			deelay = duration;
+		}
+        yield return new WaitForSecondsRealtime(fire_interv + deelay);
+		deelay = 0;
+        geyser[index].SetActive(false);
+		geyser[index].GetComponent<FireGeyser2>().damaged = false;
+        index += 1;
+        if (index == sprite_count)
+        {   
+			index = 0;
+            yield return new WaitForSecondsRealtime(duration);
+			coroutine = geyserattack(index);
+            StartCoroutine(coroutine);
+        }
+        else
+        {
+            coroutine = geyserattack(index);
+            StartCoroutine(coroutine);
+        }
+    }
 
 }
+
---
+++ JamProject/Assets/Scripts/Other Scripts/FireGeyser2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGeyser2 : MonoBehaviour {
+    private GameObject player;
+    private BoxCollider2D myCollider;
+    public bool damaged;
+    public float dmg;
+	// Use this for initialization
+	void Start () {
+		 myCollider = GetComponent<BoxCollider2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        damaged = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		   if (myCollider.IsTouchingLayers(LayerMask.GetMask("Player")) && player != null && !damaged)
+        {
+            damaged = true;
+            player.SendMessage("TakeDMG", dmg);
+            //attacking = false;
+            //Debug.Log("player damaged");
+        }
+	}
+}
