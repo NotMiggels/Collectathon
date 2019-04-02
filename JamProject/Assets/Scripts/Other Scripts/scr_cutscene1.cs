c529ad231cb7d4d31ecc39f1cd614f19ea56208f
minpark283 <parkm4@rpi.edu>
2019-04-02 10:42:50 -0400

    Added cutscene1

--- JamProject/Assets/Scripts/Other Scripts/scr_cutscene1.cs
+++ JamProject/Assets/Scripts/Other Scripts/scr_cutscene1.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cutscene1 : MonoBehaviour {
-
+public class scr_cutscene1 : MonoBehaviour {
+	public GameObject[] comic;
+	private int sprite_count;
+	private int index = 0;
 	// Use this for initialization
 	void Start () {
-		
+		sprite_count = comic.Length;
+		foreach (GameObject panel in comic){
+            panel.SetActive(false);
+        }
+		comic[0].SetActive(true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyUp("space"))
+		{
+			comic[index].SetActive(false);
+			index +=1;
+			comic[index].SetActive(true);
+
+		}
 	}
 }
