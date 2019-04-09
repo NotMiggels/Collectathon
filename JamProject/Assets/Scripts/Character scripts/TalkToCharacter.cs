89c14b6a6c06d9c79d350cc7db271ddaa1524747
Unknown <miggels97@gmail.com>
2019-04-09 16:58:39 -0400

    Added way to nope out

--- JamProject/Assets/Scripts/Character scripts/TalkToCharacter.cs
+++ JamProject/Assets/Scripts/Character scripts/TalkToCharacter.cs
@@ -32,17 +32,27 @@ public class TalkToCharacter : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.S))
             {
-                Debug.Log("space received");
+                //Debug.Log("space received");
                 talking = true;
                 gameObject.GetComponent<DialogueTrigger>().TriggerDialogue();
             }
         }
+        leftArea();
 
         // if Jelly is in the circle collider trigger of an NPC, the player should be able to talk to them by pressing a key
         // when those two conditions are met, it should trigger a function on the DialogueTrigger script called TriggerDialogue, which stores the character's dialogue
         // the trigger dialogue script will then trigger a function on DialogueManager to actually start the dialogue
 
     }
+    public bool leftArea(){
+      if (talking && !touching) {
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+
     public void DoneTalking()
     {
         talking = false;
