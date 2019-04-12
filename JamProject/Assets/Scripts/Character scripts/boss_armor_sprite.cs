c01d59b142ae6a5a329d2a715e11dfec3117c977
Jeff Yang <www.speedway@hotmail.com>
2019-04-12 01:03:33 -0400

    spatula done (I think)

--- JamProject/Assets/Scripts/Character scripts/boss_armor_sprite.cs
+++ JamProject/Assets/Scripts/Character scripts/boss_armor_sprite.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class boss_armor_sprite : MonoBehaviour
 {
     public GameObject boss;
+    public GameObject spatula_L;
+    public GameObject spatula_R;
     // Use this for initialization
     void Start()
     {
@@ -28,5 +30,17 @@ public class boss_armor_sprite : MonoBehaviour
         Debug.Log("sprite calls EndSpatula()");
         boss.GetComponent<boss_script>().ResetAttackSelection();
         boss.SendMessage("MoveToOppositeStandbyLocation");
+    }
+    public void Damage()
+    {
+        Debug.Log("spatula damage");
+        if (boss.GetComponent<boss_script>().OnLeft())
+        {
+            spatula_R.SendMessage("InflictDamage");
+        }
+        else if (boss.GetComponent<boss_script>().OnRight())
+        {
+            spatula_L.SendMessage("InflictDamage");
+        }
     }
 }
