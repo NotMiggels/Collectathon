a7c964f434655016a8a83de42d1f9a973f7a9159
minpark283 <parkm4@rpi.edu>
2019-04-12 02:11:08 -0400

    Deity have transpanrecy dending on the crispy toast count now

---
+++ JamProject/Assets/Scripts/Character scripts/deity_scr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deity_scr : MonoBehaviour {
+
+	private master_script ms;
+	// Use this for initialization
+	void Start () 
+	{
+		ms = GameObject.FindGameObjectWithTag("MasterScript").GetComponent<master_script>();
+		int ctcount = ms.getCT();
+		float inty = (float)(ctcount + 1)/21;
+		Debug.Log(inty);
+		gameObject.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f, inty);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	
+}
