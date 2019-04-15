9802c9574f0d4137bfee58b9becdc354a0b24551
minpark283 <parkm4@rpi.edu>
2019-04-15 04:24:02 -0400

    Made enemy portal scripts

---
+++ JamProject/Assets/Scripts/Other Scripts/enemyportal_scr.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyportal_scr : MonoBehaviour {
+	private List<GameObject> enemylist;
+	public GameObject[] enemyid;
+	public GameObject[] stateofportal;
+	public float delay;
+	public int spawnsize;
+	private IEnumerator coroutine;
+
+	// Use this for initialization
+	void Start () {
+		stateofportal[0].SetActive(true);
+		stateofportal[1].SetActive(false);
+		coroutine = spawnenemy(delay);
+		StartCoroutine(coroutine);
+	}
+	
+	//Spawn Random Enemy every delay seconds 
+	private IEnumerator spawnenemy(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		if(enemylist.Count < spawnsize)
+		{
+			int enemyrng =  (int)Random.Range(0, 3);
+			GameObject enemy;
+			enemy = Instantiate(enemyid[enemyrng], gameObject.transform.position, transform.rotation);
+			enemylist.Add(enemy);
+		}
+		coroutine = spawnenemy(delay);
+		StartCoroutine(coroutine);
+	}
+
+}
