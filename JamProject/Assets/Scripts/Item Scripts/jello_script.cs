15fd3238ef2c65880dbfdec1fe2fae39377b252c
Jeff Yang <www.speedway@hotmail.com>
2019-04-15 16:43:00 -0400

    Jelly fling deals damage

--- JamProject/Assets/Scripts/Item Scripts/jello_script.cs
+++ JamProject/Assets/Scripts/Item Scripts/jello_script.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class jello_script : MonoBehaviour {
-
+    private Rigidbody2D myrb;
 	// Use this for initialization
 	void Start () {
         //Debug.Log("flingy");
+        myrb = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -18,6 +19,9 @@ public class jello_script : MonoBehaviour {
     {
         if(!collision.otherCollider.isTrigger){
             Debug.Log("hit");
+            if(myrb.IsTouchingLayers(LayerMask.GetMask("Boss", "Enemy"))){
+                collision.collider.gameObject.SendMessage("TakingDMG", 5);
+            }
             //collision.gameObject.SendMessage("change");
             Destroy(this.gameObject);
         }
