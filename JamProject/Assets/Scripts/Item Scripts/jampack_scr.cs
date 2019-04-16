4b215ecd67c10f6d213ac419bf6f4e4349e75365
minpark283 <parkm4@rpi.edu>
2019-04-15 20:36:29 -0400

    Added base code for jampack

---
+++ JamProject/Assets/Scripts/Item Scripts/jampack_scr.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jampack_scr : MonoBehaviour {
+
+	private GameObject player;
+    private BoxCollider2D myCollider;
+	// Use this for initialization
+	void Start () {
+		myCollider = GetComponent<BoxCollider2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+		
+	}
+
+    //heal jelly on contact and then destroy the object	
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.gameObject.tag == "Player")
+		{
+		
+			Destroy(gameObject);
+		}
+	}
+}
